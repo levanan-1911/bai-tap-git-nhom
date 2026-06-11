e44700ad18e5706070e4011dbbfc1dbf33f123bd
NguyenMinhChien <chienday05@gmail>
2026-06-11 15:42:17 +0700

    feat: dev B cap nhat thue muoi phan phan tram

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# def tinh_thue_thu_nhap(thu_nhap):
+#     # TODO: Sinh viên sẽ cập nhật công thức tính thuế ở đây
+#     return 0
+# print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
+
 def tinh_thue_thu_nhap(thu_nhap):
-    # TODO: Sinh viên sẽ cập nhật công thức tính thuế ở đây
-    return 0
-print("Thuế phải nộp của bạn là:", tinh_thue_thu_nhap(10000000))
+    return thu_nhap * 0.1  # Giả sử thuế là 10% của thu nhập
