6f9a1b2961f37f88dad626b1a71ffc4a47f3b99b
An Le <lean80469@gmail.com>
2026-06-16 13:43:01 +0700

    feat: tối ưu lại công thức tính GPA

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def tinh_diem_gpa(diem_so):
     if diem_so >= 8.5:
         return 4.0 # Code của Dev B (Chiến)
     else:
-        return round((diem_so/10) * 4, 2) # Code của Dev A (An)
+        return round((diem_so/10) * 3.5, 2) # Code của Dev A (An)
 print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
 
 
